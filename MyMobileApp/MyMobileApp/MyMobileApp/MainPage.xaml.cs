c253ffe17984e1c10561dbe681a284d2c18bcd26
Erico Darmawan Handoyo <khe.wan.xing@outlook.com>
2018-05-08 15:00:14 +0700

    Baru nih Le

---
+++ MyMobileApp/MyMobileApp/MyMobileApp/MainPage.xaml.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace MyMobileApp
+{
+	public partial class MainPage : ContentPage
+	{
+		public MainPage()
+		{
+			InitializeComponent();
+
+            myButton.Clicked += MyButton_Clicked;
+		}
+
+        private void MyButton_Clicked(object sender, EventArgs e)
+        {
+            // Di sini Le.. pindahin tulisan yg ada di myEntry ke myLabel
+        }
+    }
+}
